ce4089ebaf72b2ec15d28a25a39878382bcaad97
pedro henrique <pedroh20066@gmail.com>
2025-09-11 11:31:46 -0300

    feat - Criando dbfactory e middleware + jwt autenticacao

---
+++ Studying-With-Future/Data/AppDbContextFactory.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Studying_With_Future.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            // Lê direto da variável de ambiente
+            var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING_GESTAO_EVENTO");
+
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException("⚠️ A variável de ambiente 'DB_CONNECTION_STRING_GESTAO_EVENTO' não foi definida!");
+
+            Console.WriteLine($"[DEBUG] Connection string usada pelo EF: {connectionString}");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
